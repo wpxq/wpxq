4dd44e79a0a44bb39b5b68cd791ca46f25347302
w` <sissypr0hyx@gmail.com>
2026-02-21 18:11:00 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,23 @@
 'use client';
 
 export default function Footer() {
-    return (
-        <footer className="py-20 mt-40 flex flex-col md:flex-row justify-between items-start md:items-end gap-10">
-          <div className="flex gap-8 font-mono text-xs uppercase tracking-widest text-gray-500">
-            <a href="https://github.com/wpxq" target="blank_" rel="noopener noreferrer" className="hover:text-white transition">Github</a>
-          </div>
-        </footer>
-    )
+  return (
+    <footer className="mt-40 pb-20 border-t border-gray-900 pt-10">
+      <div className="flex flex-col items-center justify-center gap-8">        
+        <div className="select-none pointer-events-none opacity-30">
+          <pre className="font-mono text-[20px] leading-none text-white animate-pulse tracking-[0.4em]">
+          <code>
+          {` .     +      .          * .     +      .
+            * .     +    .          * .     +
+          .     +      .          * .     +      .
+            * .     +    .          * .     +`}
+          </code>
+          </pre>
+        </div>
+        <div className="flex gap-6 text-gray-500 font-mono text-xs uppercase tracking-widest">
+          <a href="https://github.com/wpxq" target="blank_" rel="noopener noreferrer" className="hover:text-white transition-colors">Github</a>
+        </div>
+      </div>
+    </footer>
+  );
 }
