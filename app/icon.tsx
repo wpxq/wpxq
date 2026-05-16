2078d2244bfc319bac4bb115311d2b7e369e02d7
w` <217353155+wpxq@users.noreply.github.com>
2026-05-16 21:56:14 +0200

    Added: Custom Icon & Font for icon

---
+++ app/icon.tsx
@@ -0,0 +1,45 @@
+import { ImageResponse } from 'next/og'
+import fs from 'fs'
+import path from 'path'
+
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+
+export default async function Icon() {
+  const fontPath = path.join(process.cwd(), '/app/fonts/SourceCodePro-Regular.ttf')
+  const fontData = fs.readFileSync(fontPath)
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: '#000000',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: '#ffffff',
+          fontSize: 22,
+          fontFamily: 'SourceCodePro',
+          paddingBottom: '2px',
+        }}
+      >
+        W
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: 'SourceCodePro',
+          data: fontData,
+          style: 'normal',
+          weight: 400,
+        },
+      ],
+    }
+  )
+}
